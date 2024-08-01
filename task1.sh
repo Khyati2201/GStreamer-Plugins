669f05177cb7d2bd3cc111370744abd9523a6404
Khyati <khyati@intelligentedgesystems.com>
2024-08-02 00:40:33 +0530

    Add files via upload

---
+++ task1.sh
@@ -0,0 +1,1 @@
+gst-launch-1.0 filesrc location=/home/khyati/Downloads/test_video.mp4 ! qtdemux name=demux demux. ! queue ! faad ! audioconvert ! audioresample ! autoaudiosink demux. ! queue ! avdec_h264 ! videoconvert ! autovideosink
