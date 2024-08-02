009aed072dba9191834ced99ea934fd0c0b5aa54
Khyati <khyati@intelligentedgesystems.com>
2024-08-02 07:43:52 +0530

    Update task1.sh

--- task1.sh
+++ task1.sh
@@ -1,1 +1,1 @@
-gst-launch-1.0 filesrc location=/home/khyati/Downloads/test_video.mp4 ! qtdemux name=demux demux. ! queue ! faad ! audioconvert ! audioresample ! autoaudiosink demux. ! queue ! avdec_h264 ! videoconvert ! autovideosink
+gst-launch-1.0 filesrc location=location_to_test_video ! qtdemux name=demux demux. ! queue ! faad ! audioconvert ! audioresample ! autoaudiosink demux. ! queue ! avdec_h264 ! videoconvert ! autovideosink
