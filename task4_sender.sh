2eca6fb31a7b02ff31a527d071513fc52a60b68b
Khyati <khyati@intelligentedgesystems.com>
2024-08-02 07:47:05 +0530

    Update task4_sender.sh

--- task4_sender.sh
+++ task4_sender.sh
@@ -1,1 +1,1 @@
-gst-launch-1.0 filesrc location=/home/khyati/Downloads/test_video.mp4 ! qtdemux name=demux demux.video_0 ! h264parse ! avdec_h264 ! videoconvert ! x264enc ! rtph264pay ! udpsink host=127.0.0.1 port=5000
+gst-launch-1.0 filesrc location=location_to_test_video ! qtdemux name=demux demux.video_0 ! h264parse ! avdec_h264 ! videoconvert ! x264enc ! rtph264pay ! udpsink host=127.0.0.1 port=5000
